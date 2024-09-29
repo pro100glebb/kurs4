76dc547a336631609aafe8700a2b4369a4226828
Gleb Kozin <glebkozin7@gmail.com>
2024-09-29 00:48:02 -0700

    Дописал класс vacancy

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,27 +1,55 @@
+import json
 
 class Vacancy():
     """Класс для работы с вакансиями"""
-    def __init__(self, name: str, url: str, salary: int | None, description: str | None):
-        self.name = name
+    def __init__(self, name: str, area: str, salary: int, url: str, snippet: str):
+        self.name = self.__validation_data(name)
+        self.area = self.__validation_data(area)
+        self.salary = salary
         self.url = url
-        self.salary = self.validate(self, salary)
-        self.description = description
+        self.snippet = snippet
 
-    @staticmethod
-    def validate(self, salary: int | None) -> int:
-        return salary or 0
+    def __str__(self):
+        return (f"{self.name}\n"
+                f"Город: {self.area}\n"
+                f"Зарплата: {self.salary if self.salary else "Не указана"}\n"
+                f"Ссылка: {self.url}\n"
+                f"Сниппет: {self.snippet}\n")
 
     def __lt__(self, other):
-        return self.salary < other.salary
+        if not self.salary:
+            return "Зарплата не указана в первой вакансии"
+        elif not other.salary:
+            return "Зарплата не указана во второй вакансии"
+        elif self.salary < other.salary:
+            return True
+        else:
+            return False
 
-    def __repr__(self):
-        pass
-    def toJSON(self):
-        return json.dumps(
-            self,
-            default=lambda o: o.__dict__,
-            sort_keys=True,
-            indent=4)
+
+    @staticmethod
+    def __validation_data(data):
+        if data:
+            return data
+        else:
+            return "Отсутствует"
+
+    @classmethod
+    def new_vacancy(cls, vacancy):
+        name = vacancy.get("name")
+        area = vacancy.get("area").get("name")
+        if vacancy.get("salary"):
+            salary = vacancy.get("salary").get("from")
+        else:
+            salary = 0
+        url = vacancy.get("url")
+        snippet = vacancy.get("snippet").get("requirement")
+        return cls(name, area, salary, url, snippet)
+
+
+
+
+
 
 
 
